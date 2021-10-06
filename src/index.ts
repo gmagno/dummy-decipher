bb060d263bb2165bb2d12c1b67321a24c71872eb
Goncalo <goncalo.magno@gmail.com>
2021-10-06 14:50:37 -0300

    Add file flag

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import {createDecipheriv} from 'crypto'
 
+let fs = require('fs')
+
 class Decipher extends Command {
   static description = 'Yet another decipher'
 
@@ -9,6 +11,7 @@ class Decipher extends Command {
     help: flags.help({char: 'h'}),
     key: flags.string({char: 'k', description: 'key'}),
     data: flags.string({char: 'd', description: 'encrypted data'}),
+    file: flags.string({char: 'f', description: 'path to file with encrypted data'}),
   }
 
   static args = [{name: 'file'}]
@@ -16,15 +19,30 @@ class Decipher extends Command {
   async run() {
     const {args, flags} = this.parse(Decipher)
 
-    const key = flags.key
-    const data = flags.data
+    // const key = flags.key
+    // const data = flags.data
+    // const file = flags.file
+    let fileData
 
-    if (key && data) {
-      const decryptedData = this.decipherData(data, key)
-      this.log(decryptedData)
-    } else {
-      this.log('-k or -d flags are missing...')
+    if (!flags.key) {
+      this.error('Missing key flag (-k)')
+      return
     }
+    if (!flags.data && !flags.file) {
+      this.error('Missing data (-d) and file (-f) flags. Please provide at least one.')
+    }
+
+    let data
+
+    if (flags.file) {
+      fileData = fs.readFileSync(flags.file, 'utf8')
+      data = fileData.toString()
+    }
+    if (flags.data) {
+      data = flags.data
+    }
+    const decryptedData = this.decipherData(data, flags.key)
+    this.log(decryptedData)
   }
 
   decipherData(encryptedText: string, key: string): string {
